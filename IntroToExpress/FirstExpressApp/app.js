2c0034344798c2864c8374520ea2927ef74d0c74
Sandeep PS <pssandeep@gmail.com>
2018-04-02 22:04:41 +1000

    Intro to Express - Params

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -14,6 +14,18 @@ app.get("/bye",function(req,res){
 	res.send("<H1>Good Bye</H1>");
 });
 
+
+app.get("/params/:parameters",function(req,res){
+
+	res.send("<H1>" + req.params.parameters + "</H1>");
+});
+
+app.get("*",function(req,res){
+
+	res.send("<H1>Wrong Page</H1>");
+});
+
+
 app.listen("3000", function(){
 	console.log("Server started at port 3000...");
 
