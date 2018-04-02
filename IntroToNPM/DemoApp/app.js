99c67dce32cbe9ad3644cab036d7c7b2f5c127ef
Sandeep PS <pssandeep@gmail.com>
2018-04-02 16:09:38 +1000

    Intro To NPM

---
+++ IntroToNPM/DemoApp/app.js
@@ -0,0 +1,8 @@
+var catme = require("cat-me");
+var knock = require("knock-knock-jokes");
+
+console.log("cat-me ");
+console.log(catme());
+
+console.log("Knock-Knock-Jokes ");
+console.log(knock());
