eb6d4b043895580476a90776d7c57853f00ee210
Sandeep PS <pssandeep@gmail.com>
2018-04-11 00:09:16 +1000

    YelpCamp Add CampGround

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+var urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
+
+app.use(urlencodedParser);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -21,5 +27,20 @@ var campgrounds= [
 app.get("/campgrounds",function(req,res){
 
     res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.get("/campgrounds/new", function(req,res){
+
+    res.render("new");
+});
+
+app.post("/campgrounds", function(req,res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampGround = {name : name, image : image};
+
+    campgrounds.push(newCampGround);
+
+    res.redirect("/campgrounds");
 });
 app.listen("3000", () => console.log("YelpCap Server started on Port #3000"));
