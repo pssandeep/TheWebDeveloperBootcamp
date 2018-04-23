8a947e1913034e3316594f0ea4cb2afa784cb06a
Sandeep PS <pssandeep@gmail.com>
2018-04-23 22:19:01 +1000

    Authentication Demo

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,9 +1,34 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const User = require("./models/user");
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
 
+mongoose.connect("mongodb://localhost/auth_demo");
+
+app.use(require("express-session")({
+   secret : "a" ,
+   resave : false,
+   saveUninitialized : false
+}));
+app.use(urlencodedParser);
 app.set("view engine", "ejs");
+app.use(passport.initialize());
+app.use(passport.session());
 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+// All Routes
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -11,5 +36,21 @@ app.get("/", (req, res) => {
 
 app.get("/secret", (req, res) => {
     res.render("secret");
+});
+
+app.get("/register", (req,res) => {
+    res.render("register")
+});
+
+app.post("/register", (req,res) => {
+    User.register(new User({username : req.body.name}), req.body.password, (err,user) => {
+        if(err){
+            console.log(err);
+            return res.render("register");
+        } 
+        passport.authenticate("local")(req,res, () => {
+            res.redirect("/secret");
+        });
+    });
 });
 app.listen("3000", () => console.log("Server started at port #3000..."));
--- Authentication/AuthDemo/models/user.js
+++ Authentication/AuthDemo/models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 mongoose.connect("mongodb://localhost/auth_demo");
 const userSchema = new mongoose.Schema({
-    name : {type : String}
+    name : {type : String},
+    passport : {type : String}
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
