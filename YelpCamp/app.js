2d9f58c179253b30450d25ca75743ce8a10e6a72
Sandeep PS <pssandeep@gmail.com>
2018-04-25 15:00:34 +1000

    Add Authentication to App. Style to Login and Register

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -27,9 +27,9 @@ seedDB();
 
 // Passport Configuration
 app.use(require("express-session")({
-    secret : "a",
-    resave : false,
-    saveUninitialized : false
+    secret: "a",
+    resave: false,
+    saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -37,9 +37,13 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// index route. To display all the campgrounds
-app.get("/campgrounds", function (req, res) {
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
 
+// index route. To display all the campgrounds
+app.get("/campgrounds", isLoggedIn, function (req, res) {
     CampGround.find({}, function (err, campgrounds) {
         if (err) {
             console.log(err);
@@ -55,19 +59,19 @@ app.get("/campgrounds", function (req, res) {
 // All Routes
 
 //home route
-app.get("/", function (req, res) {
+app.get("/", isLoggedIn, function (req, res) {
 
     res.render("home");
 });
 
 // get route to show the form to enter new camp grounds.
-app.get("/campgrounds/new", function (req, res) {
+app.get("/campgrounds/new", isLoggedIn, function (req, res) {
 
     res.render("campgrounds/new");
 });
 
 //show route. Show more details about the campgrounds
-app.get("/campgrounds/:id", function (req, res) {
+app.get("/campgrounds/:id", isLoggedIn, function (req, res) {
     CampGround.findById(req.params.id).populate("comments").exec(function (err, campground) {
         if (err) {
             console.log(err);
@@ -81,7 +85,7 @@ app.get("/campgrounds/:id", function (req, res) {
 });
 
 //post route. Add new campgrounds to database
-app.post("/campgrounds", function (req, res) {
+app.post("/campgrounds", isLoggedIn, function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
@@ -106,7 +110,7 @@ app.post("/campgrounds", function (req, res) {
 //Comment Routes
 
 //Comments - Add New Comments
-app.get("/campgrounds/:id/comments/new", function (req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function (req, res) {
     CampGround.findById(req.params.id, function (err, campground) {
         if (err) {
             console.log(err);
@@ -121,7 +125,7 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
 });
 
 //Comments - Create new Comments
-app.post("/campgrounds/:id/comments", function (req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function (req, res) {
     CampGround.findById(req.params.id, function (err, campground) {
         if (err) {
             console.log(err);
@@ -140,22 +144,49 @@ app.post("/campgrounds/:id/comments", function (req, res) {
 
 //Auth Routes
 
-app.get("/register", (req,res) => {
+app.get("/register", (req, res) => {
     res.render("auth/register");
 });
 
-app.post("/register", (req,res) => {
-    var newUser= new User({username : req.body.username});
-    User.register(newUser, req.body.password, (err,user) => {
-        if(err){
+app.post("/register", (req, res) => {
+    var newUser = new User({
+        username: req.body.username
+    });
+    User.register(newUser, req.body.password, (err, user) => {
+        if (err) {
             console.log(err);
             return res.render("auth/register");
         }
-        passport.authenticate("local")(req,res, () => {
+        passport.authenticate("local")(req, res, () => {
             res.redirect("/campgrounds");
         });
     });
 });
 
+app.get("/login", (req, res) => {
+    res.render("auth/login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), (req, res) => {
+
+});
+
+
+app.get("/logout", (req, res) => {
+    req.logOut();
+    res.redirect("/login");
+});
+
 //server port listening at Port 3000
 app.listen("3000", () => console.log("YelpCap Server started on Port #3000"));
+
+//Middleware functions
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect("/login");
+}
